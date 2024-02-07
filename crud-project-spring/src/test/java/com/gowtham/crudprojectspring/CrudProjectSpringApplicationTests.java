bdc942cdcf13b9078b67fb7443cce1e24db6fede
GOWTHAMS03 <gowthammani00007@gmail.com>
2024-02-07 08:12:52 +0530

    code init

---
+++ crud-project-spring/src/test/java/com/gowtham/crudprojectspring/CrudProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gowtham.crudprojectspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
