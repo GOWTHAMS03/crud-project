3aa8599ef236b10865ffaa8bd8f38ade17a018d6
GOWTHAMS03 <gowthammani00007@gmail.com>
2024-02-07 09:05:41 +0530

    update spring code

--- crud-project-spring/src/main/java/com/gowtham/crudprojectspring/CrudProjectSpringApplication.java
+++ crud-project-spring/src/main/java/com/gowtham/crudprojectspring/CrudProjectSpringApplication.java
@@ -2,8 +2,10 @@ package com.gowtham.crudprojectspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+
 public class CrudProjectSpringApplication {
 
 	public static void main(String[] args) {
